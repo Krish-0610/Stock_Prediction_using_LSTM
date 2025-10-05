324e1474fee6d6e2010afa064b53735c45100ca4
Banti <bantikushwaha7984@gmail.com>
2025-10-06 00:13:23 +0530

    Dashboard added

--- backend/stock_api/stock_api/settings.py
+++ backend/stock_api/stock_api/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'stock_api.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
