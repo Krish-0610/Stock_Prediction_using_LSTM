9cb35546b7de2a2e6a17f8229df73968b39fc59d
Krish Patel <kdp88532@gmail.com>
2026-01-16 21:20:25 +0530

    Add macro features to model and update the structure

--- backend/stock_api/predictions/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import ChartDataAPI, ModelEvaluationAPI, PredictionAPI
-
-urlpatterns = [
-    path('predict/', PredictionAPI.as_view(), name='predict-price'),
-    path('chart/<str:period>/', ChartDataAPI.as_view(), name='chart-data'),
-    path('evaluate/', ModelEvaluationAPI.as_view(), name='evaluate-model'),
-]
